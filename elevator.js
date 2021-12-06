092773dfccf5c2f5d807222d5b227d549a6c91c4
yehudafridman <yehudafridman>
2021-12-06 12:05:20 +0200

    update

--- elevator.js
+++ elevator.js
@@ -1,7 +1,18 @@
 
 function create() {
-    let w = document.querySelector("#id_input_building").value
-    for (let i = 0; i < w; i++) {
+    let TotalBuildings = document.querySelector("#id_input_building").value
+
+    if (TotalBuildings <= 0) {
+        alert("אנא הכנס מספר בניינים גדול מ 0")
+    }
+
+    let i = 0;
+    while (document.getElementById(`all_site-${i}`)) {
+        document.getElementById(`all_site-${i++}`).remove();
+    }
+
+    for (let i = 0; i < TotalBuildings;
+        i++) {
         let queues = {};
         let Orders = [];
         createFloors(i, queues, Orders)
@@ -11,85 +22,84 @@ function create() {
 }
 
 let SeveralFloors = 20
-let SeveralElevators = 5
-function createFloors(bildingNumber, queues, Orders) {
+let SeveralElevators = 3
+function createFloors(buildingNumber, queues, Orders) {
 
     let all_site = document.createElement("div");
-    all_site.id = `all_site-${bildingNumber}`;
+    all_site.id = `all_site-${buildingNumber}`;
     all_site.style.display = "inline-block";
     main.appendChild(all_site);
 
     let floors = document.createElement("div");
-    floors.id = `floors-${bildingNumber}`;
+    floors.id = `floors-${buildingNumber}`;
     floors.style.float = "left"
-    document.getElementById(`all_site-${bildingNumber}`).appendChild(floors);
+    document.getElementById(`all_site-${buildingNumber}`).appendChild(floors);
 
     for (let i = SeveralFloors; i > 0; i--) {
         let newDiv = document.createElement("div");
         newDiv.className = "floor";
-        newDiv.id = `floor${i}-${bildingNumber}`;
+        newDiv.id = `floor${i}-${buildingNumber}`;
         newDiv.style.height = "103px"
         floors.appendChild(newDiv);
 
-        let tymer = document.createElement("div");
-        tymer.className = "tymer";
-        tymer.id = `tymer${i - 1}-${bildingNumber}`;
-        document.getElementById(`floor${i}-${bildingNumber}`).appendChild(tymer);
+        let time = document.createElement("div");
+        time.className = "time";
+        time.id = `time${i - 1}-${buildingNumber}`;
+        document.getElementById(`floor${i}-${buildingNumber}`).appendChild(time);
 
         let newBut = document.createElement("button");
         newBut.className = "metal linear";
         newBut.innerHTML = i - 1;
-        newBut.id = `button${i - 1}-${bildingNumber}`;
+        newBut.id = `button${i - 1}-${buildingNumber}`;
 
-        newBut.onclick = (e) => { fillQueues(e, bildingNumber, queues, Orders) }
-        document.getElementById(`floor${i}-${bildingNumber}`).appendChild(newBut);
+        newBut.onclick = (e) => { fillQueues(e, buildingNumber, queues, Orders) }
+        document.getElementById(`floor${i}-${buildingNumber}`).appendChild(newBut);
 
     }
-    document.getElementById(`floor${SeveralFloors}-${bildingNumber}`).style.borderTop = "none"
+    document.getElementById(`floor${SeveralFloors}-${buildingNumber}`).style.borderTop = "none"
 
 }
 
 
 
-function createElevator(bildingNumber, queues, Orders) {
+function createElevator(buildingNumber, queues, Orders) {
 
     let elevators = document.createElement("div");
-    elevators.id = `elevators-${bildingNumber}`;
+    elevators.id = `elevators-${buildingNumber}`;
     elevators.style.float = "right"
-    // elevators.style.top=" 0px"
-    document.getElementById(`all_site-${bildingNumber}`).appendChild(elevators);
+    document.getElementById(`all_site-${buildingNumber}`).appendChild(elevators);
 
     for (let i = 0; i < SeveralElevators; i++) {
 
         queues[i] = [];
         let newElv = document.createElement("img");
-        newElv.id = `elv${i}-${bildingNumber}`
+        newElv.id = `elv${i}-${buildingNumber}`
         newElv.src = "elv.png";
         elevators.appendChild(newElv);
         newElv.style.top = (Math.abs(110 * (SeveralFloors - 1)) - 7) + "px";
 
     }
-    setInterval(() => { qMng(bildingNumber, queues, Orders) }, 500)
+    setInterval(() => { qMng(buildingNumber, queues, Orders) }, 500)
 
 }
 
 
 
-function fillQueues(e, bildingNumber, queues, Orders) {
+function fillQueues(e, buildingNumber, queues, Orders) {
     if (Orders.indexOf(Number(e.target.innerText)) === -1) {
         let bestWaiting, bestElevator;
         let len = Object.keys(queues).length;
 
         for (let i = 0; i < len; i++) {
-            let distens = 0.5 * Number(e.target.innerText);
+            let distance = 0.5 * Number(e.target.innerText);
             let waiting = 0;
 
             if (queues[i].length > 0) {
                 waiting = queues[i].at(-1)[1];
-                distens = Math.abs(queues[i].at(-1)[0] - Number(e.target.innerText)) * 0.5
+                distance = Math.abs(queues[i].at(-1)[0] - Number(e.target.innerText)) * 0.5
             }
-            if (bestWaiting == undefined || bestWaiting > distens + waiting) {
-                bestWaiting = distens + waiting;
+            if (bestWaiting == undefined || bestWaiting > distance + waiting) {
+                bestWaiting = distance + waiting;
                 bestElevator = i;
             }
 
@@ -97,11 +107,10 @@ function fillQueues(e, bildingNumber, queues, Orders) {
         Orders.push(Number(e.target.innerText))
         queues[bestElevator].push([e.target.innerText, bestWaiting + 2])
         e.target.style.color = "#b00";
-        // document.getElementById(`button${[e.target.innerText]}`).style.color = "#b00";
     }
 }
 
-function qMng(bildingNumber, queues, Orders) {
+function qMng(buildingNumber, queues, Orders) {
     for (let i = 0; i < Object.keys(queues).length; i++) {
 
         if (queues[i].length > 0 && queues[i][0][1] == 2) {
@@ -109,27 +118,27 @@ function qMng(bildingNumber, queues, Orders) {
         }
 
         if (queues[i].length > 1 && queues[i][0][1] == 0) {
-            document.getElementById(`elv${i}-${bildingNumber}`).style.transition = `top ${queues[i][1][1] - 2}s linear`
-            document.getElementById(`elv${i}-${bildingNumber}`).style.top = Math.abs(110 * (SeveralFloors - 1)) - 7 - (queues[i][1][0]) * 110 + "px"
- 
+            document.getElementById(`elv${i}-${buildingNumber}`).style.transition = `top ${queues[i][1][1] - 2}s linear`
+            document.getElementById(`elv${i}-${buildingNumber}`).style.top = Math.abs(110 * (SeveralFloors - 1)) - 7 - (queues[i][1][0]) * 110 + "px"
+
             let remove = Orders.indexOf(Number(queues[i][0][0]))
             Orders.splice(remove, 1)
             queues[i].shift()
         }
 
         else if (queues[i].length == 1) {
-            document.getElementById(`elv${i}-${bildingNumber}`).style.transition = `top ${queues[i][0][1] - 2}s linear`
-            document.getElementById(`elv${i}-${bildingNumber}`).style.top = Math.abs(110 * (SeveralFloors - 1)) - 7 - (queues[i][0][0]) * 110 + "px"
+            document.getElementById(`elv${i}-${buildingNumber}`).style.transition = `top ${queues[i][0][1] - 2}s linear`
+            document.getElementById(`elv${i}-${buildingNumber}`).style.top = Math.abs(110 * (SeveralFloors - 1)) - 7 - (queues[i][0][0]) * 110 + "px"
         }
 
         for (let j = 0; j < queues[i].length; j++) {
 
             if (queues[i][j][1] >= 2) {
-                document.getElementById(`tymer${[queues[i][j][0]]}-${bildingNumber}`).innerHTML = (queues[i][j][1] - 2);
+                document.getElementById(`time${[queues[i][j][0]]}-${buildingNumber}`).innerHTML = (queues[i][j][1] - 2);
             }
             if (queues[i][j][1] < 2) {
-                document.getElementById(`tymer${[queues[i][j][0]]}-${bildingNumber}`).innerHTML = "";
-                document.getElementById(`button${[queues[i][j][0]]}-${bildingNumber}`).style.color = "";
+                document.getElementById(`time${[queues[i][j][0]]}-${buildingNumber}`).innerHTML = "";
+                document.getElementById(`button${[queues[i][j][0]]}-${buildingNumber}`).style.color = "";
             }
             if (queues[i][j][1] > 0) {
                 queues[i][j][1] = queues[i][j][1] - 0.5;
